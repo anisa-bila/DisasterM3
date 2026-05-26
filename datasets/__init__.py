2b4c74767befb7f80ed5d228eb3c80db7f49287a
anisa-bila <anisakharirasalsabila@gmail.com>
2026-05-26 12:59:54 +0300

    Update __init__.py

--- datasets/__init__.py
+++ datasets/__init__.py
@@ -1,1 +1,7 @@
+from datasets.disasterm3 import DisasterM3Dataset
 
+def build_dataset_config(dataset_name, project_root, subset):
+    if "disasterm3" in dataset_name.lower():
+        return DisasterM3Dataset(project_root, subset)
+    else:
+        raise NotImplementedError(dataset_name)
