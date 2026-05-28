70a00886d05770793dee815a259beadc5694ba48
anisa-bila <anisakharirasalsabila@gmail.com>
2026-05-28 10:06:48 +0300

    Update base.py

--- datasets/base.py
+++ datasets/base.py
@@ -1,3 +1,5 @@
+# Parent class that all dataset classes must follow. Forces dataset classes to have a load() function, similar to ModelConfig.
+
 class BaseDataset:
   def load(self):
     raise NotImplementedError
